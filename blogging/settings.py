a89348cdad0eef8f032cd28e4498d3d58e304c61
anurag singh <anurag6569201@gmail.com>
2024-04-08 12:50:52 +0530

    updated gmail smtp service

--- blogging/settings.py
+++ blogging/settings.py
@@ -35,13 +35,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-EMAIL_HOST = 'smtp-relay.brevo.com'
-# EMAIL_HOST_USER = 'anurag6569201@gmail.com'
-# EMAIL_HOST_PASSWORD = 'BG3KhIQHmgYZzbja'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-
-EMAIL_HOST_USER = 'b323009@iiit-bh.ac.in'
-EMAIL_HOST_PASSWORD = 'SJD2vkI3B4Nn5j0g'
+EMAIL_HOST_USER = 'anurag6569201@gmail.com'
+EMAIL_HOST_PASSWORD = 'ihasreqxxrrtzmrc'
+EMAIL_USE_TLS=True
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 
 # Application definition
 
