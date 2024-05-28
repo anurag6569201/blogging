bcbddb59992cd82dc8343c9dea81cf797a7160f7
anurag singh <anurag6569201@gmail.com>
2024-05-29 00:53:44 +0530

    updated vercel settings

--- blogging/settings.py
+++ blogging/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','.vercel.app', '.now.sh']
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
