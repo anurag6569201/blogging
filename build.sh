67bec4b43fd799d3cc2e88e23e6606291509cd03
anurag singh <anurag6569201@gmail.com>
2024-05-29 10:20:57 +0530

    updated build

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install django
 
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
