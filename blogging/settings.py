4dfdf289ce4d0864fc904d054793d690feb5aa2e
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-06-21 01:43:19 +0530

    on debug

--- blogging/settings.py
+++ blogging/settings.py
@@ -31,7 +31,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = 'django-insecure-vfs3$v(l4$k1%%mtexoyy0+(%%z(f26-*d2sb9n82_qmp%+63n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://blogsingh.azurewebsites.net','https://127.0.0.1']
